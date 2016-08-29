84c2f9dd238afb15f8142cae9d5ba77b84174277
Javier <schjavier@gmail.com>
2016-08-29 11:04:34 -0300

    Update ej6.py

--- ej6.py
+++ ej6.py
@@ -1,10 +1,16 @@
+#-*- encoding: utf-8 -*-
+
 def factorial():
 	"""
-	Pide un numero natural n y calcula su factorial!
+	
+	Pide un numero natural n y calcula su factorial.
+	No recibe argumentos de entrada y no posee valores de salida.
 
 	"""
-	n = int(input ("ingrese un numero natural por favor: "))
-	fact = 1 #determinamos el valor de inicializacion en 1
+	print ("================BIENVENIDO A NUESTRO PROGRAMA!================ \nCon él, podrás saber el factorial de cualquier número natural! \n")
+	
+	n = int(input ("Ingrese un número natural por favor: "))
+	fact = 1 #determinamos el valor de inicialización en 1
 	
 	for x in range (1 , n +1):
 		fact = fact * x
